28295165e6885497bfc05a6218dd65575718a10d
Mauro Gonzalo Gutierrez <maurog7gool@hotmail.com>
2022-01-03 00:14:41 -0300

    Retos Solucionados y Finalizados

--- js/punto10.js
+++ js/punto10.js
@@ -1,10 +1,8 @@
 let five = 5;
-let Multiplicador = 1;
 console.log(`La tabla del 5:`);
 
 for (i = 1; i <= 10; i++) {
-  console.log(`5 x ${Multiplicador} =`);
-  five = 5 * Multiplicador;
-  Multiplicador++;
+  console.log(`5 x ${i} =`);
+  five = 5 * i;
   console.log (five)
 }
--- js/punto11.js
+++ js/punto11.js
@@ -1,5 +1,4 @@
 const num = parseInt(prompt("Ingrese un numero entre 1 y 10, y se mostrara la tabla que desea saber"));
-let multiplicador = 1
 console.log (`Se mostrara la tabla del ${num}:`)
 
 for (i = 1; i <= 13; i++) {
